f7b5bb8116dc40413a95686da06f259e6b19d28c
zpf0918 <zhanggongzifei@163.com>
2017-07-15 14:06:14 +0800

    implement 4-6

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,13 +1,30 @@
 class Api::V1::ReservationsController < ApiController
 
+  def index
+    @reservations = current_user.reservations
+
+    render :json => {
+      :data => @reservations.map { |reservation|
+        {
+          :booking_code => reservation.booking_code,
+          :train_number => reservation.train.number,
+          :seat_number => reservation.seat_number,
+          :customer_name => reservation.customer_name,
+          :customer_phone => reservation.customer_phone
+        }
+      }
+    }
+  end
+
   def create
-    @train = Train.find_by_number(params[:train_number])
+    @train = Train.find_by_number!(params[:train_number])
     @reservation = Reservation.new( :train_id => @train.id,
                                     :booking_code => params[:booking_code],
                                     :seat_number => params[:seat_number],
                                     :customer_name => params[:customer_name],
                                     :customer_phone => params[:customer_phone]
                                     )
+    @reservation.user = current_user
     if @reservation.save
       render :json => {
         :booking_code => @reservation.booking_code,
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get "/trains" => "trains#index", as: :trains
       get "/trains/:number" => "trains#show", as: :train
 
+      get "/reservations" => "reservations#index", as: :reservations
       get "/reservations/:booking_code" => "reservations#show", as: :reservation
       post "reservations" => "reservations#create", as: :create_reservations
       patch "reservations/:booking_code" => "reservations#update", as: :update_reservation
